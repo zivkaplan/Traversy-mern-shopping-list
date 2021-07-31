6a3ebea29180153261c44d7ca7df985000727183
Ziv Kaplan <zivkaplan@gmail.com>
2021-07-31 13:24:40 +0300

    client navbar

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container,
+} from 'reactstrap';
+
+export default class AppNavbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { isOpen: false };
+        this.toggle = this.toggle.bind(this);
+    }
+    toggle() {
+        this.setState({ isOpen: !this.state.isOpen });
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar color="dark" dark expand="sm" className="mb-5">
+                    <Container>
+                        <NavbarBrand href="/">MERN Shopping List</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="ms-auto" navbar>
+                                <NavItem>
+                                    <NavLink href="https://github.com/zivkaplan">
+                                        Github
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+        );
+    }
+}
